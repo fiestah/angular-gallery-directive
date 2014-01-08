a0c6f4b2efa9564f6418a24bc21072809ff44b4b
Marvin Tam <marvin@fiestah.com>
2014-01-08 17:09:57 -0500

    v0.3.0
    - Handle pre-rendered html (as opposed to using ng-repeat)
    - Handle margins between items
    - Much more concise directive APIs

--- README.md
+++ README.md
@@ -1,20 +1,39 @@
-Basic image gallery built on AngularJS. Likely IE8+ only. Requires jQuery just because of element.width(). 
+# angular-gallery-directive
 
-[Full Demo](http://jsfiddle.net/thatmarvin/FhjBv/embedded/result/).
+This is a basic SEO-friendly gallery built on AngularJS. Unlike Bootstrap's carousel component, this works with html rendered on the server-side (instead of `ng-repeat`) so that search bots can crawl the gallery content. You may still use `ng-repeat` to render if you don't care about that.
+
+It depends on jQuery for things like `element.position()` and `element.height()`. Likely IE8+ only.
+
+<!-- [Full Demo](http://jsfiddle.net/thatmarvin/FhjBv/embedded/result/). -->
+
 
 ## Directives
-
-### Elements/Attributes:
 All are required to except for `gallery-indicators`
 
-- `gallery`: The root scope of the gallery. 
-    - `gallery-width` & `gallery-height`: Sets the visible area of the gallery. Include the units ('200px', '100%' etc.)
-    - `gallery-item-width`: Register the width of each gallery item, in pixels
-- `gallery-items`: Tell the gallery which is the container element for all the gallery images/items
-- `gallery-indicators`: _optional_ This generates the regular little round dots on galleries that tell you where in the gallery you're at. Click on it to scroll to the associated item.
+- `gallery`: _attribute_ The root scope of the gallery.
+- `gallery-window`: _attribute_ This "window" element should wrap `gallery-items` so that it limits the visible portion of the "film strip" by hiding the overflow.
+- `gallery-item-list`: _attribute_ The "film strip" of the gallery, which is the parent element for all the gallery image/item elements.
+- `gallery-indicators`: _element/attribute_ _optional_ This generates the typical little round dots on galleries that tell you where in the gallery you're at. Click on it to scroll to the associated item.
 
-### `$scope` functions
-You can call this anywhere within `gallery` scope in the HTML:
+
+## CSS and Classes
+You'll need this [_minimal_ amount of CSS](src/angular-gallery-directive.css) to get it working. All classes below are required to hook up the CSS:
+
+- `.gallery-window`: The "window" that wraps the `.gallery-item-list`
+- `.gallery-item-list`: The "filmstrip" that contains a bunch of `.gallery-item`s.
+- `.gallery-item`: The item in the gallery. Could contain `<img>`s or any arbitrary html.
+
+If you want margins between items, just add them in CSS:
+
+``` css
+.gallery-item + .gallery-item { margin-left: 20px; }
+```
+
+\* While it is possible add these classes via JS by default, you'd likely still want them in there on load anyway to minimize FOUC before JS kicks in.
+
+
+## `$scope` functions
+Call these anywhere within the `gallery` scope:
 
 ### Scroll one item at a time:
 - `scrollToIndex(index)`: Scroll to the numbered item
@@ -28,79 +47,49 @@ You can call this anywhere within `gallery` scope in the HTML:
 - `previousScreen()`
 
 
-### CSS and Classes
-You'll need this [_minimal_ amount of CSS](src/angular-gallery-directive.css) to get it working. All classes below are required to hook up the CSS:
-
-- `.gallery-item`: The item in the gallery. Could contain `<img>`s or any arbitrary html.
-- `.gallery-item-list`: The "filmstrip" that contains a bunch of `.gallery-item`s.
-- `.gallery-window`: The "window" that wraps the `.gallery-item-list`
-
-
-## Barebones Example ([Fiddle](http://jsfiddle.net/thatmarvin/DDRVC/)):
-First, pass in an array of objects (containing like image urls, captions, etc.) into `$scope.items` in the controller.
-
-Set this variable directly:
-
-``` js
-  function GalleryController($scope) {
-    $scope.items = [
-      {url: '/1.jpg'},
-      {url: '/2.jpg'}
-     ];
-  }
-```
-
-… or use an async service like `$http`:
-
-``` js
-  function GalleryController($scope, $http) {
-    $http.get('/images.json').then(function (response) {
-      // Promises are awesome
-      $scope.items = response.data;
-    });
-  }
-```
-
-
+## Barebones Example:
 Your HTML should look something like:
 
 ``` html
-  <div gallery
-    gallery-width="300px"
-    gallery-height="200px"
-    gallery-item-width='300'
-    ng-controller="GalleryController" class="gallery">
-      <!-- The images -->
-      <div class="gallery-window">
-          <ul gallery-images class="gallery-item-list">
-              <li ng-repeat="item in items" class="gallery-item">
-                  <img ng-src="{{item.url}}" width="300" height="200">
-              </li>
-          </ul>
-      </div>
+  <div gallery class="gallery">
+    <!-- The images -->
+    <div gallery-window class="gallery-window">
+      <ul gallery-item-list class="gallery-item-list">
+        <li class="gallery-item">
+           <img src="/photo-1.jpg" width="300" height="200" alt="">
+        </li>
+        <li class="gallery-item">
+           <img src="/photo-2.jpg" width="300" height="200" alt="">
+        </li>
+        <li class="gallery-item">
+           <img src="/photo-3.jpg" width="300" height="200" alt="">
+        </li>
+      </ul>
+    </div>
 
-      <!-- The little dots that tell you where you are -->
-      <div gallery-indicators></div>
+    <!-- The little dots that tell you where you are -->
+    <div gallery-indicators></div>
 
-      <!-- Nav actions -->
-      <div class="gallery-nav">
-        <a ng-click="previousItem()">Previous</a>
-        <a ng-click="nextItem()">Next</a>
-      </div>
+    <!-- Nav actions -->
+    <div class="gallery-nav">
+      <a ng-click="previousItem()">Previous</a>
+      <a ng-click="nextItem()">Next</a>
+    </div>
   </div>
 ```
 
-Because AngularJS is awesome, you can markup the gallery items however you want (say, add captions to photos) as long as the directives and class names are in the right spots.
+Because AngularJS is awesome, you can markup the gallery however you want (say, add captions to the photos or order elements differently) as long as the directives and class names are at the right places.
 
 Similarly for navigation, just trigger `previousItem()/previousScreen()` or `nextItem()/nextScreen()` using directives like `ng-click` wherever you feel like it.
 
 
 ## Notes:
-- This uses `display: inline-block` to inline the gallery items, so it does not like white spaces between `.gallery-item`s, and does not like IE7 and below very much.
+- This uses `display: inline-block` to inline the gallery items, so watch out for white spaces/line breaks between `.gallery-item`s, which will introduce extra space between the them.
 
 
 ## Todos:
 - Tests
 - Support `ng-animate`
+- Support `controller as` syntax
 - Keyboard nav support
 - Figure out default/custom templates
--- src/angular-gallery-directive.js
+++ src/angular-gallery-directive.js
@@ -3,118 +3,122 @@ angular.module('fiestah.gallery', [])
 // Container directive for the items, navigation and nav dots container
 // elements
 .directive('gallery', function () {
-  return {
-    restrict: 'EA',
-    controller: [
-      '$scope', '$element', '$attrs', '$window',
-      function ($scope, $element, $attrs, $window) {
-        // Parse gallery height and width
-        $scope.WIDTH = $attrs.galleryWidth;
-        $scope.HEIGHT = $attrs.galleryHeight;
-        $scope.selectedIndex = 0;
-        $scope.selectedScreen = 0;
+  function link(scope, element, attrs) {
+    scope.selectedIndex  = 0;
+    scope.selectedScreen = 0;
 
-        var itemWidth = parseInt($attrs.galleryItemWidth, 10);
 
-        // Get the number of fully-visible items in the gallery container
-        function getNumPerScreen() {
-          var viewportWidth = $element.width();
-          return Math.floor(viewportWidth / itemWidth);
+    // If item elements are built using ng-repeat, they won't be rendered
+    // yet at the time of directive compilation. This is why getItems() and
+    // functions depending on it cannot be called immediately on load.
+    function getItems() {
+      return element.find('[gallery-item-list]').children();
+    }
+
+    // Get the offset position of a particular item
+    function getItemOffset(index) {
+      return getItems()[index].offsetLeft;
+    }
+
+    // Get the number of fully-visible items in the gallery container
+    function getNumPerScreen() {
+      // Get dimensions on demand to account for screen resizes
+      var visibleWidth = element.width();
+      var items        = getItems();
+
+      for (var i=0; i<items.length; i++) {
+        if (getItemOffset(i) + $(items[i]).width() > visibleWidth) {
+          return i;
         }
-        var numPerScreen = getNumPerScreen();
-
-        // Adjust numPerScreen when the window gets resized if the gallery
-        // width is not in fixed units
-        // @todo: debounce the resize event
-        if (/(%|em)$/.test($scope.WIDTH)) {
-          angular.element($window).bind('resize', function () {
-            numPerScreen = getNumPerScreen();
-          });
-        }
-
-
-        // Scroll by individual items
-        $scope.scrollToItem = function (index) {
-          $scope.selectedIndex = index;
-
-          $scope.position = {
-            left: -(itemWidth * index) + 'px'
-          };
-        };
-
-        $scope.previousItem = function () {
-          var numOfItems = $scope.items.length;
-          var index = ($scope.selectedIndex === 0)
-            ? $scope.items.length - 1
-            : $scope.selectedIndex - 1;
-
-          // Going from the first item to the last
-          $scope.isWrappingPrevious = ($scope.selectedIndex === 0)
-            && index === numOfItems - 1;
-          $scope.isWrapping = $scope.isWrappingPrevious;
-
-          $scope.scrollToItem(index);
-        };
-
-        $scope.nextItem = function () {
-          // Going from the last item to the first
-          var numOfItems = $scope.items.length;
-          $scope.isWrappingNext = ($scope.selectedIndex === numOfItems - 1)
-            && index === 0;
-          $scope.isWrappingPrevious = $scope.isWrappingNext;
-
-          $scope.scrollToItem(($scope.selectedIndex + 1) % $scope.items.length)
-        };
-
-
-        // Scroll by collection of items
-        $scope.scrollToScreen = function (screenIndex) {
-          var itemIndex = (screenIndex * numPerScreen);
-          var numOfScreens = Math.ceil($scope.items.length / numPerScreen);
-
-          $scope.selectedScreen = screenIndex;
-
-          $scope.position = {
-            left: -(itemWidth * numPerScreen * screenIndex) + 'px'
-          };
-        };
-
-        $scope.previousScreen = function () {
-          var numOfScreens = Math.ceil($scope.items.length / numPerScreen);
-          var index = ($scope.selectedScreen === 0)
-            ? numOfScreens - 1
-            : $scope.selectedScreen - 1;
-
-          // Going from the first screen to the last
-          $scope.isWrappingPrevious = ($scope.selectedScreen === 0)
-            && index === numOfScreens - 1;
-          $scope.isWrapping = $scope.isWrappingPrevious;
-
-          $scope.scrollToScreen(index);
-        };
-
-        $scope.nextScreen = function () {
-          var numOfScreens = Math.ceil($scope.items.length / numPerScreen);
-          var index = ($scope.selectedScreen + 1) % numOfScreens;
-
-          // Going from the last screen to the first
-          $scope.isWrappingNext = ($scope.selectedScreen === numOfScreens - 1)
-            && index === 0;
-          $scope.isWrapping = $scope.isWrappingNext;
-
-          $scope.scrollToScreen(index);
-        };
-
       }
-    ]
+    }
+
+
+    // Scroll by individual items
+    scope.scrollToItem = function (index) {
+      scope.selectedIndex = index;
+      scope.position = {
+        left: -(getItemOffset(index)) + 'px'
+      };
+    };
+
+    scope.previousItem = function () {
+      var numOfItems = getItems().length;
+      var index = (scope.selectedIndex === 0)
+        ? numOfItems - 1
+        : scope.selectedIndex - 1;
+
+      // Going from the first item to the last
+      scope.isWrappingPrevious = (scope.selectedIndex === 0)
+        && index === numOfItems - 1;
+      scope.isWrapping = scope.isWrappingPrevious;
+
+      scope.scrollToItem(index);
+    };
+
+    scope.nextItem = function () {
+      // Going from the last item to the first
+      var numOfItems = getItems().length;
+      scope.isWrappingNext = (scope.selectedIndex === numOfItems - 1);
+      scope.isWrapping = scope.isWrappingNext;
+
+      scope.scrollToItem((scope.selectedIndex + 1) % numOfItems)
+    };
+
+
+    // Scroll by collection of items
+    scope.scrollToScreen = function (screenIndex) {
+      var numOfItems = getItems().length;
+      var numPerScreen = getNumPerScreen();
+      var itemIndex = (screenIndex * numPerScreen);
+      var numOfScreens = Math.ceil(numOfItems / numPerScreen);
+
+      scope.selectedScreen = screenIndex;
+
+      scope.position = {
+        left: -(getItemOffset(numPerScreen * screenIndex)) + 'px'
+      };
+    };
+
+    scope.previousScreen = function () {
+      var numOfItems = getItems().length;
+      var numOfScreens = Math.ceil(numOfItems / getNumPerScreen());
+      var index = (scope.selectedScreen === 0)
+        ? numOfScreens - 1
+        : scope.selectedScreen - 1;
+
+      // Going from the first screen to the last
+      scope.isWrappingPrevious = (scope.selectedScreen === 0)
+        && index === numOfScreens - 1;
+      scope.isWrapping = scope.isWrappingPrevious;
+
+      scope.scrollToScreen(index);
+    };
+
+    scope.nextScreen = function () {
+      var numOfItems = getItems().length;
+      var numOfScreens = Math.ceil(numOfItems / getNumPerScreen());
+      var index = (scope.selectedScreen + 1) % numOfScreens;
+
+      // Going from the last screen to the first
+      scope.isWrappingNext = (scope.selectedScreen === numOfScreens - 1)
+        && index === 0;
+      scope.isWrapping = scope.isWrappingNext;
+
+      scope.scrollToScreen(index);
+    };
   }
+
+  return {
+    restrict: 'A',
+    link: link
+  };
 })
 
 // Directive that shows the position of the currently selected item
 .directive('galleryIndicators', function () {
   return {
     restrict: 'EA',
-    require: '^gallery',
     template: '<a'
       + ' class="indicator"'
       + ' ng-repeat="item in items"'
@@ -124,25 +128,41 @@ angular.module('fiestah.gallery', [])
   };
 })
 
-// Directive for container that contains all the scrollable items
-.directive('galleryItems', function () {
+// The visible area of the "window" that frames the "film strip"
+.directive('galleryWindow', ['$interval', function ($interval) {
   return {
-    restrict: 'EA',
-    require: '^gallery',
-    link: function (scope, el, attrs) {
-      // Frames the currently selected item on the film strip
-      el.parent().css({
-        width: scope.WIDTH,
-        height: scope.HEIGHT
-      });
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      // Frames the currently selected item on the film strip. Polling here is
+      // needed when waiting for ng-repeat to finish rendering so we can set
+      // the visible area to the height of the film strip
+      var filmStripEl = $(element.children()[0]);
+      var i = 0;
+      var poll = $interval(function () {
+        var height = filmStripEl.height();
+        i++; // Keep a counter to give up after 5s
 
+        if (height > 0 || i >= 20) {
+          element.css({ height: height });
+          $interval.cancel(poll);
+        }
+      }, 250);
+    }
+  };
+}])
+
+// Directive for the "film strip" parent element which contains all the
+// scrollable items
+.directive('galleryItemList', function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
       // Updates the posiiton of the film strip
       scope.$watch('position', function () {
-        el.css(scope.position || {
+        element.css(scope.position || {
           left: 0
         });
       });
-
     }
   };
 });
